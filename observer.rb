cba94f8de157b7ff8e95c52faf878dce40b95182
Yuka Sato <yuka.sato@gaiax.com>
2017-08-05 16:58:02 +0900

    use module

--- observer.rb
+++ observer.rb
@@ -1,16 +1,6 @@
-class Employee
-  attr_reader :name, :title, :salary
-
-  def initialize(name, title, salary)
-    @name = name
-    @title = title
-    @salary = salary
+module Subject
+  def initialize
     @observers = []
-  end
-
-  def salary=(new_salary)
-    @salary = new_salary
-    notify_observers
   end
 
   def notify_observers
@@ -25,6 +15,24 @@ class Employee
 
   def delete_observer(observer)
     @observers.delete(observer)
+  end
+end
+
+class Employee
+  include Subject
+
+  attr_reader :name, :title, :salary
+
+  def initialize(name, title, salary)
+    super()
+    @name = name
+    @title = title
+    @salary = salary
+  end
+
+  def salary=(new_salary)
+    @salary = new_salary
+    notify_observers
   end
 end
 
